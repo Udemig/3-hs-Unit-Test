5d5270b5ef855367f41a5caf021dd69ba56a73e1
furkanevin <furkanevin00@gmail.com>
2023-02-18 22:03:02 +0300

    veri çekme ve sepet güncelleme eklendi

--- src/pages/wecome-page/Scoops.jsx
+++ src/pages/wecome-page/Scoops.jsx
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from 'react';
 
 const Scoops = () => {
   const [cesitler, setCesitler] = useState([]);
+  const [sepet, setSepet] = useState([]);
 
   useEffect(() => {
     axios
@@ -15,13 +16,27 @@ const Scoops = () => {
       });
   }, []);
 
+  // SIFIRLAMA İŞLEMİ
+
+  const handleReset = (param) => {
+    const reseted = sepet.filter((i) => i.name !== param.name);
+    setSepet(reseted);
+  };
+
+  //ADET BULMA FONK.
+  const findAmount = (param) => {
+    const bulundu = sepet.filter((i) => i.name === param.name);
+    return bulundu.length;
+  };
+
   return (
     <div>
       <h1 className="text-start">Dondurma Çeşitleri</h1>
       <p className="text-start">Tanesi 3$</p>
-      <h2 className="text-start">Çeşitler Ücreti: 0</h2>
+      <h2 className="text-start">Çeşitler Ücreti: {sepet.length * 3} $</h2>
       <div className="row mt-4 d-flex gap-4 p-3">
         {cesitler.map((cesit) => {
+          const adet = findAmount(cesit);
           return (
             <div
               className="col-3 d-flex flex-column align-items-center"
@@ -33,13 +48,22 @@ const Scoops = () => {
                 src={cesit.imagePath}
                 alt="cesit"
               />
-              {/*TODO ALT TAG */}
               <label className="lead">{cesit.name}</label>
 
               <div className="d-flex gap-2 mt-2 align-items-center">
-                <button className="btn btn-danger">Sıfırla</button>
-                <span className="lead">0</span>
-                <button className="btn btn-warning">Ekle</button>
+                <button
+                  className="btn btn-danger"
+                  onClick={() => handleReset(cesit)}
+                >
+                  Sıfırla
+                </button>
+                <span className="lead">{adet}</span>
+                <button
+                  className="btn btn-warning"
+                  onClick={() => setSepet([...sepet, cesit])}
+                >
+                  Ekle
+                </button>
               </div>
             </div>
           );
--- src/pages/wecome-page/tests/Cesitler.test.jsx
+++ src/pages/wecome-page/tests/Cesitler.test.jsx
@@ -1,5 +1,6 @@
 import { findAllByRole, render, screen } from '@testing-library/react';
 import Scoops from './../Scoops';
+import userEvent from '@testing-library/user-event';
 /*
 !SEÇİCİLER
 ? Komut [All] BySeçici
@@ -9,9 +10,39 @@ import Scoops from './../Scoops';
 ! query => elementler DOM da yok ise  
 */
 
-test('API den gelen veri her çeşit için ekrana basılır', async () => {
+test('API den gelen veriyi her çeşit objesi için ekrana basar', async () => {
   render(<Scoops />);
 
   //çeşitlerde resimleri çek
   const resimler = await screen.findAllByRole('img', { name: 'cesit' }); //selectors
+  expect(resimler).toHaveLength(4);
+});
+
+test('Dondurma çeşitlerinde ekleme ve sıfırlama işlemi', async () => {
+  render(<Scoops />);
+  const user = userEvent.setup();
+
+  // gerekli elemanları alalım
+  const toplamH1 = screen.getByRole('heading', {
+    name: /Çeşitler Ücreti: /i,
+  });
+
+  //ekle butonlarını çekme
+  const ekleBtns = await screen.findAllByRole('button', { name: /ekle/i });
+
+  //Sıfırlama butonlaarını çekme
+  const delBtns = await screen.findAllByRole('button', { name: /Sıfırla/i });
+
+  //Ekleme işlemleri ve kontrolü
+  await user.click(ekleBtns[0]);
+
+  expect(toplamH1).toHaveTextContent('3');
+
+  await user.dblClick(ekleBtns[1]);
+
+  expect(toplamH1).toHaveTextContent('9');
+
+  //Sıfırlama işlemi
+  await user.click(delBtns[1]);
+  expect(toplamH1).toHaveTextContent('3');
 });
